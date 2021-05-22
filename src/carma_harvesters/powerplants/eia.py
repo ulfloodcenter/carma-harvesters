6093d57186b70ebb41ab7b53520b94068c7d6096
Brian Miles <selimnairb@gmail.com>
2021-05-21 20:15:36 -0400

    CN-57: Updated implementation of power plant location lookup based on public EIA data

--- src/carma_harvesters/powerplants/eia.py
+++ src/carma_harvesters/powerplants/eia.py
@@ -1,14 +1,16 @@
-import sqlite3
-import csv
 import os
 import pkg_resources
 import logging
+
+from shapely.geometry.base import BaseGeometry
+
+from ..util import select_points_contained_by_geometry
 
 
 logger = logging.getLogger(__name__)
 
 CARMA_HARVESTERS_RSRC_KEY = 'carma_harvesters'
-EIA_FORM_860_SCHEDULE_2_PATH = 'data/2___Plant_Y2019.csv'
+EIA_FORM_860_SCHEDULE_2_PATH = 'data/2___Plant_Y2019.gpkg'
 
 
 class PowerPlantLocations:
@@ -19,66 +21,5 @@ class PowerPlantLocations:
             raise Exception("EIA FORM 860 schedule 2 power plant location file cannot be found or cannot be read.")
         logger.debug(f"Plant location input path: {self.plant_loc_in_path}")
 
-        # Create in-memory database
-        self.conn = sqlite3.connect(':memory:')
-        # Enable Spatialite extension (so that we can do spatial queries)
-        self.conn.enable_load_extension(True)
-        self.conn.execute('SELECT load_extension("mod_spatialite")')
-        self.conn.enable_load_extension(False)
-        self.conn.execute('SELECT InitSpatialMetaData()')
-
-        # Create table to hold plant location data
-        cur = self.conn.cursor()
-        cur.execute('''CREATE TABLE IF NOT EXISTS eia_form860_plant_loc
-        (
-        plant_code INTEGER PRIMARY KEY,
-        longitude TEXT,
-        latitude TEXT
-        )
-        ''')
-        cur.execute('''SELECT AddGeometryColumn
-        (
-        'eia_form860_plant_loc',
-        'geom_nad83',
-        4269,
-        'POINT',
-        'XY',
-        0
-        )
-        ''')
-        cur.execute('''SELECT CreateSpatialIndex
-        (
-        'eia_form860_plant_loc',
-        'geom_nad83'
-        )
-        ''')
-
-        # Read plant data from CSV into database
-        with open(self.plant_loc_in_path, 'r') as plant_loc_file:
-            plant_loc_reader = csv.reader(plant_loc_file, delimiter=',')
-            for row_num, row in enumerate(plant_loc_reader, start=1):
-                if row_num > 2:
-                    plant_id = int(row[2])
-                    long = row[10]
-                    lat = row[9]
-                    try:
-                        geom_text = f"POINT({long} {lat})"
-                        cur.execute("""INSERT INTO eia_form860_plant_loc
-                        (plant_code, longitude, latitude, geom_nad83)
-                        VALUES (?, ?, ?, GeomFromText(?, 4269))""",
-                                    (plant_id, long, lat, geom_text))
-                    except sqlite3.IntegrityError as e:
-                        logger.error(("Unable to load plant location data, values where: "
-                                      f"plant_id: {plant_id}, long: |{long}|, lat: |{lat}|, geom_text: {geom_text}")
-                                     )
-                        raise e
-        cur.close()
-
-    def get_lon_lat_for_plant(self, plant_code: int) -> (str, str):
-        cur = self.conn.cursor()
-        cur.execute("""SELECT longitude, latitude 
-        FROM eia_form860_plant_loc
-        WHERE plant_code=? 
-        """,
-                    (plant_code,))
-        return cur.fetchone()
+    def get_plants_within_geometry(self, geom: BaseGeometry) -> dict:
+        return select_points_contained_by_geometry(self.plant_loc_in_path, geom)
--- src/tests/unit/test_eia.py
+++ src/tests/unit/test_eia.py
@@ -1,17 +1,37 @@
 import unittest
 
+from shapely.geometry import asShape
+
 from carma_harvesters.powerplants.eia import PowerPlantLocations
 
 
 class TestPowerPlantLocations(unittest.TestCase):
-    def test_powerplant_locations(self):
+    def test_powerplant_locations_geom(self):
+        # -93.8562,29.9930,-91.0107,32.6024
+        test_geojson = {
+            "type": "Polygon",
+            "coordinates": [[ [-95.8562, 28.9930], 
+                              [-95.8562, 32.6024], 
+                              [-91.0107, 32.6024],
+                              [-91.0107, 29.9930],
+                              [-95.8562, 29.9930] ]]
+        }
+        test_geom = asShape(test_geojson)
+
         loc = PowerPlantLocations()
         self.assertIsNotNone(loc)
 
-        response = loc.get_lon_lat_for_plant(63924)
-        self.assertIsNotNone(response)
-        self.assertEqual('-85.654444', response[0])
-        self.assertEqual('40.294347', response[1])
+        la_plants = loc.get_plants_within_geometry(test_geom)
+        features = la_plants['features']
+        # There should be 92 points in the above polygon
+        self.assertEqual(92, len(features))
+        # First plant
+        self.assertEqual(6055, features[0]['properties']['Plant Code'])
+        self.assertEqual(-91.3692, features[0]['geometry']['coordinates'][0])
+        self.assertEqual(30.7261, features[0]['geometry']['coordinates'][1])
+        # Last plant
+        self.assertEqual(54240, features[-1]['properties']['Plant Code'])
+        self.assertEqual(-92.6497, features[-1]['geometry']['coordinates'][0])
+        self.assertEqual(32.5256, features[-1]['geometry']['coordinates'][1])
 
-        response = loc.get_lon_lat_for_plant(6392400)
-        self.assertIsNone(response)
+
